5a257e2168a13df7e732f34c2c850816d7bf5ace
Nonwhiskerscat <nonwhiskerscat@gmail.com>
2022-12-02 21:31:52 +0900

    ver3.0

--- js/attackNumber.js
+++ js/attackNumber.js
@@ -1,10 +1,12 @@
 function attackNumber(cat) {
 
     const nyan = document.querySelector('.kitty');
+    const nyans= document.querySelectorAll('.kitty #catnum');
 
     let attack=[];
     for(let i=0;i<cat;i++) {
         let meow = Number(nyan.querySelector(`.num_${i+1}`).value);
+        if(isNaN(meow)||meow=='') meow='X';
         attack.push(meow);
     }
     
